3bef9693824961ad0a47f2960a76a0c715b13377
j <74ef54cf@opayq.com>
2017-11-11 14:20:27 -0800

    squash! squash! join-pane binding

--- scripts/join_pane.sh
+++ scripts/join_pane.sh
@@ -4,20 +4,9 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source "$CURRENT_DIR/helpers.sh"
 
-create_new_window() {
- TMUX="" tmux -S "$(tmux_socket)" new-window -P -F
-}
-
-new_window_pane_id() {
-  local new_window_id="$1"
-  tmux list-panes -t "$new_window_id" -F "#{pane_id}"
-}
-
 join_pane() {
-  local new_window_id="$(create_new_window)"
-  local new_window_pane_id="$(new_window_pane_id "$new_window_id")"
-  tmux join-pane -t "$new_window_pane_id"
-  # tmux kill-pane -t "$new_window_pane_id"
+  tmux join-pane
+  tmux break-pane
 }
 
 main() {
