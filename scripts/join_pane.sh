81423990d936f1afb6f12f8ba6434df37940a72c
j <74ef54cf@opayq.com>
2017-11-15 01:02:14 -0800

    switch-client 2-key bindings

--- scripts/join_pane.sh
+++ scripts/join_pane.sh
@@ -1,47 +1,51 @@
 #!/usr/bin/env bash
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-JOIN_KEY="$1"
+
+# where the secondary key bindings are bound
+SECONDARY_KEY_TABLE="join-pane"
+
+# more of a convenient sentinel for the full screen join
+BREAK_PANE_FLAG="-b"
+
+# passed to script on the 2-key binding invocation
+JOIN_PANE_FLAG="$1"
 
 source "$CURRENT_DIR/helpers.sh"
-
-join_prompt() {
-  tmux command-prompt -1 \
-    -p 'join-pane - horizontal: (h % |) vertical: (v " -) full: (f @) ' \
-    "run \"'$CURRENT_DIR/join_pane.sh' '%%%'\""
-}
 
 join_pane() {
   local join_pane_flag="$1"
 	tmux join-pane "$join_pane_flag" || \
     tmux display-message "Mark a(nother) pane first"
 
-  if [ "-b" == "$join_pane_flag" ]; then
+  if [ "$BREAK_PANE_FLAG" == "$join_pane_flag" ]; then
    tmux break-pane || true
   fi
 }
 
-main() {
-  if [ -z "$JOIN_KEY" ]; then
-    join_prompt
-  else
-    local join_pane_flag
-    case $JOIN_KEY in
-      'h') join_pane_flag="-h";;
-      '%') join_pane_flag="-h";;
-      '|') join_pane_flag="-h";;
-      'v') join_pane_flag="-v";;
-      '"') join_pane_flag="-v";;
-      '-') join_pane_flag="-v";;
-      'f') join_pane_flag="-b";;
-      '@') join_pane_flag="-b";;
-    esac
+bind_secondary_keys() {
+  while read -r key flag; do
+    tmux bind-key -T"$SECONDARY_KEY_TABLE" "$key" \
+         run "'$CURRENT_DIR/join_pane.sh' '$flag'"
+  done <<KEY_FLAGS
+h -h
+% -h
+| -h
+v -v
+" -v
+- -v
+f $BREAK_PANE_FLAG
+@ $BREAK_PANE_FLAG
+KEY_FLAGS
+}
 
-    if [ -z "$join_pane_flag" ]; then
-      tmux display-message "invalid key"
-    else
-      join_pane "$join_pane_flag"
-    fi
+main() {
+  if [ -z "$JOIN_PANE_FLAG"]; then
+    bind_secondary_keys
+    tmux switch-client -T"$SECONDARY_KEY_TABLE"
+  else
+    join_pane "$JOIN_PANE_FLAG"
   fi
+
 }
 main
