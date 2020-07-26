0c0a309698a6a6418d92a2f3a404b08372435cd5
Albert <aface@aface.pro>
2020-07-26 14:59:24 +0500

    Match sessions list with fzf

--- scripts/goto_session.sh
+++ scripts/goto_session.sh
@@ -3,9 +3,6 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 main() {
-	# displayes tmux session list
-	tmux run-shell "$CURRENT_DIR/list_sessions.sh"
-	# goto command prompt
-	tmux command -p session: "run '$CURRENT_DIR/switch_or_loop.sh \"%1\"'"
+	tmux run-shell -b "$CURRENT_DIR/list_sessions.sh | fzf-tmux | xargs $CURRENT_DIR/switch_or_loop.sh"
 }
 main
