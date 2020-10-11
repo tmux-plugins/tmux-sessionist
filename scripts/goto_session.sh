f4e55f1780462c1de1d5b6aa25d92ef799f96c49
Mohammed Alzakariya <lanhikarixx@gmail.com>
2020-10-10 23:52:22 -0500

    Update fzf goto-sesson not to signal Ctrl+C when invoked

--- scripts/goto_session.sh
+++ scripts/goto_session.sh
@@ -3,6 +3,6 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 main() {
-	tmux run-shell -b "$CURRENT_DIR/list_sessions.sh | fzf-tmux | xargs $CURRENT_DIR/switch_or_loop.sh"
+	tmux run-shell -b "$CURRENT_DIR/list_sessions.sh | fzf-tmux | xargs tmux switch-client -t || true"
 }
 main
