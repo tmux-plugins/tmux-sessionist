9d8680d358826ff8d6a2989054340be84cb86452
Mohammed Alzakariya <lanhikarixx@gmail.com>
2020-10-11 00:10:07 -0500

    Display goto-session fzf in its own pane
    Credits: brought script from tmux-fzf: https://github.com/sainnhe/tmux-fzf

--- scripts/goto_session.sh
+++ scripts/goto_session.sh
@@ -3,6 +3,6 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 main() {
-	tmux run-shell -b "$CURRENT_DIR/list_sessions.sh | fzf-tmux | xargs tmux switch-client -t || true"
+	tmux run-shell -b "$CURRENT_DIR/list_sessions.sh | $CURRENT_DIR/fzf-tmux-pane.sh | xargs tmux switch-client -t || true"
 }
 main
