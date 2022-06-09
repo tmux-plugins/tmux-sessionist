ab3e4e04cc3c7dab91b8e6d1044b175807ea808f
Bruno Sutic <code@brunosutic.com>
2022-06-09 13:17:51 +0200

    Improve goto session performance

--- scripts/goto_session.sh
+++ scripts/goto_session.sh
@@ -3,8 +3,8 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 main() {
-	# displayes tmux session list
-	tmux run-shell "$CURRENT_DIR/list_sessions.sh"
+	# displays tmux session list
+	tmux run-shell -b "$CURRENT_DIR/list_sessions.sh"
 	# goto command prompt
 	tmux command -p session: "run '$CURRENT_DIR/switch_or_loop.sh \"%1\"'"
 }
