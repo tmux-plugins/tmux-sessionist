9ffd53074ce984db1e1ff2323e24123881c43c0b
TheSast <27977196+TheSast@users.noreply.github.com>
2023-05-10 22:18:31 +0200

    refactor(promotion): use IDs instead of names
    should prevent any possible ambiguity caused by using names

--- scripts/promote_pane.sh
+++ scripts/promote_pane.sh
@@ -5,31 +5,33 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 
 # global vars passed to the script as arguments
-CURRENT_SESSION_NAME="$1"
+CURRENT_SESSION_ID="$1"
 CURRENT_PANE_ID="$2"
-PANE_CURRENT_PATH="$3"
+PANE_CURRENT_PATH="$4"
 
 number_of_panes() {
-	tmux list-panes -s -t "$CURRENT_SESSION_NAME" |
+	tmux list-panes -s -t "$CURRENT_SESSION_ID" |
 		wc -l |
 		tr -d ' '
 }
 
 create_new_session() {
-	TMUX="" tmux -S "$(tmux_socket)" new-session -c "$PANE_CURRENT_PATH" -d -P -F "#{session_name}"
+	TMUX="" tmux -S "$(tmux_socket)" new-session -c "$PANE_CURRENT_PATH" -d -P -F "#{session_id}"
 }
 
 new_session_pane_id() {
-	local session_name="$1"
-	tmux list-panes -t "$session_name" -F "#{pane_id}"
+	local session_id="$1"
+	tmux list-panes -t "$session_id" -F "#{pane_id}"
+}
+
 }
 
 promote_pane() {
-	local session_name="$(create_new_session)"
-	local new_session_pane_id="$(new_session_pane_id "$session_name")"
+	local session_id="$(create_new_session)"
+	local new_session_pane_id="$(new_session_pane_id "$session_id")"
 	tmux swap-pane -s "$CURRENT_PANE_ID" -t "$new_session_pane_id"
 	tmux kill-pane -t "$new_session_pane_id"
-	switch_to_session "$session_name"
+	switch_to_session "$session_id"
 }
 
 main() {
--- scripts/promote_window.sh
+++ scripts/promote_window.sh
@@ -5,32 +5,34 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 
 # global vars passed to the script as arguments
-CURRENT_SESSION_NAME="$1"
+CURRENT_SESSION_ID="$1"
 CURRENT_WINDOW_ID="$2"
 CURRENT_WINDOW_NAME="$3"
 WINDOW_CURRENT_PATH="$4"
 
 number_of_windows() {
-	tmux list-windows -t "$CURRENT_SESSION_NAME" |
+	tmux list-windows -t "$CURRENT_SESSION_ID" |
 		wc -l |
 		tr -d ' '
 }
 
 create_new_session() {
-	TMUX="" tmux -S "$(tmux_socket)" new-session -c "$WINDOW_CURRENT_PATH" -s "$CURRENT_WINDOW_NAME" -d -P -F "#{session_name}"
+	TMUX="" tmux -S "$(tmux_socket)" new-session -c "$WINDOW_CURRENT_PATH" -d -P -F "#{session_id}"
 }
 
 new_session_window_id() {
-	local session_name="$1"
-	tmux list-windows -t "$session_name" -F "#{window_id}"
+	local session_id="$1"
+	tmux list-windows -t "$session_id" -F "#{window_id}"
+}
+
 }
 
 promote_window() {
-	local session_name="$(create_new_session)"
-	local new_session_window_id="$(new_session_window_id "$session_name")"
+	local session_id="$(create_new_session)"
+	local new_session_window_id="$(new_session_window_id "$session_id")"
 	tmux swap-window -s "$CURRENT_WINDOW_ID" -t "$new_session_window_id"
 	tmux kill-window -t "$new_session_window_id"
-	switch_to_session "$session_name"
+	switch_to_session "$session_id"
 }
 
 main() {
